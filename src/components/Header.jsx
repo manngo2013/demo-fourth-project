4f6320d97ed30d8e13cfcb5437f9afcc97e6fed4
manngo <manngo2013@gmail.com>
2023-06-23 15:38:29 +0700

    Lecture 42: Using Link NavLink and Active Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
-import "./Header.scss";
+import { FaUser } from "react-icons/fa";
 import { MdHome } from "react-icons/md";
 import { RiLock2Fill } from "react-icons/ri";
-import { FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
 
 function Header(props) {
   return (
@@ -23,35 +23,35 @@ function Header(props) {
             <Button variant="outline-success">Search</Button>
           </Form>
           <Nav className="me-auto">
-            <Link to="/" className="nav-link">
+            <NavLink to="/" className="nav-link">
               <MdHome style={{ fontSize: "22px" }} /> Home
-            </Link>
-            <Link to="/about" className="nav-link">
+            </NavLink>
+            <NavLink to="/about" className="nav-link">
               About Us
-            </Link>
-            <Link to="/service" className="nav-link">
+            </NavLink>
+            <NavLink to="/service" className="nav-link">
               Service
-            </Link>
-            <Link to="/works" className="nav-link">
+            </NavLink>
+            <NavLink to="/works" className="nav-link">
               Our Works
-            </Link>
-            <Link to="/news" className="nav-link">
+            </NavLink>
+            <NavLink to="/news" className="nav-link">
               News & Events
-            </Link>
-            <Link to="/contact" className="nav-link">
+            </NavLink>
+            <NavLink to="/contact" className="nav-link">
               Contact Us
-            </Link>
+            </NavLink>
           </Nav>
 
           <Nav>
-            <Link to="/login" className="nav-link">
+            <NavLink to="/login" className="nav-link">
               <RiLock2Fill />
               Login
-            </Link>
-            <Link to="/register" className="nav-link">
+            </NavLink>
+            <NavLink to="/register" className="nav-link">
               <FaUser />
               Register
-            </Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
